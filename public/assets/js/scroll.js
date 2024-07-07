58b3a7168bd23271f1abb95464d09a0343c1b05e
YOHAN ALEK PLAZOLA ARANGURE <yh9dev@gmail.com>
2024-07-06 22:42:58 -0700

    Se adaptó la sección de proyectos

--- public/assets/js/scroll.js
+++ public/assets/js/scroll.js
@@ -11,3 +11,7 @@ ScrollReveal().reveal('.header', {delay: 0, origin: 'top', distance: '0px'});
 ScrollReveal().reveal('.home__title', {delay: 0, origin: 'left'});
 ScrollReveal().reveal('.home__desc', {delay: 100, origin: 'left'});
 ScrollReveal().reveal('.home__buttons', {delay: 200, origin: 'left'});
+
+ScrollReveal().reveal('.proyects h1', {delay: 0, origin: 'top'});
+ScrollReveal().reveal('.proyects h2', {delay: 0, origin: 'top'});
+ScrollReveal().reveal('.proyect', {delay: 0, origin: 'top'});
